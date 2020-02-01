23e29130976e22fdff918615299dea81d057f54b
Rajan Garg <45937605+rajngarg@users.noreply.github.com>
2020-02-01 17:48:36 +0530

    Added Date Converter class

---
+++ Utils.java
@@ -0,0 +1,44 @@
+package com.codingeagles.storyshow;
+
+import java.util.Date;
+
+public class Utils {
+
+    public static String getDurationBetweenDates(Date d1, Date d2) {
+
+        long diff = d1.getTime() - d2.getTime();
+        long seconds = diff / 1000;
+        long minutes = seconds / 60;
+        long hours = minutes / 60;
+        long days = hours / 24;
+
+        StringBuilder formattedDiff = new StringBuilder();
+        if (days != 0) {
+            if (Math.abs(days) == 1)
+                return formattedDiff.append(Math.abs(days) + " day ago").toString();
+            else
+                return formattedDiff.append(Math.abs(days) + " days ago").toString();
+        }
+        if (hours != 0) {
+            if (Math.abs(hours) == 1)
+                return formattedDiff.append(Math.abs(hours) + " hour ago").toString();
+            else
+                return formattedDiff.append(Math.abs(hours) + " hours ago").toString();
+        }
+        if (minutes != 0) {
+            if (Math.abs(minutes) == 1)
+                return formattedDiff.append(Math.abs(minutes) + " minute ago").toString();
+            else
+                return formattedDiff.append(Math.abs(minutes) + " minutes ago").toString();
+        }
+        if (seconds != 0) {
+            if (Math.abs(seconds) == 1)
+                return formattedDiff.append(Math.abs(seconds) + " second ago").toString();
+            else
+                return formattedDiff.append(Math.abs(seconds) + " seconds ago").toString();
+        }
+
+        return "";
+    }
+
+}
